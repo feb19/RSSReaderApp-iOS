87bee7baf8568587d35a3779e2b60563f3efde49
Nobuhiro Takahashi <nt@feb19.jp>
2018-06-24 18:10:37 +0900

    Initial Commit

---
+++ RSSReaderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSReaderApp
+//
+//  Created by TakahashiNobuhiro on 2018/06/24.
+//  Copyright © 2018 feb19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
